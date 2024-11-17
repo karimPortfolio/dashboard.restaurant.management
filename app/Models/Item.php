01bad22b073b09c636679ed801a136884a15963f
KarimPortfolio <mohamedkarmballa19@gmail.com>
2024-11-17 18:56:37 +0100

    Added migrations and models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Item extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function menus():BelongsToMany
+    {
+        return $this->belongsToMany(Menu::class, 'menu_items');
+    }
+
+    public function ingredients():BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class, 'item_ingredients');
+    }
 }
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -10,4 +10,15 @@ class Menu extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function items()
+    {
+        return $this->belongsToMany(Item::class, 'menu_items');
+    }
+
+    public function createBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
 }
